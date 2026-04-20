56bbd95ecd4eb5d9222828782ecd8a254643f578
Miguel Angel Guzman Palma <mguzman.izalco@gmail.com>
2026-04-20 13:13:12 -0600

    Update ventas-app.jsx

--- ventas-app.jsx
+++ ventas-app.jsx
@@ -16,7 +16,7 @@ const T = {
 };
 
 const SUCURSALES_DEFAULT = ["GARAN 1","GARAN 2","GARAN 7"];
-const CV_DEFAULT = ["DESAYUNO","ALMUERZO","REFRIGERIO MAÑANA","REFRIGERIO TARDE","CENA","EVENTOS"];
+const CV_DEFAULT = ["DESAYUNO","ALMUERZO","REFRIGERIO MAÑANA","REFRIGERIO TARDE"];
 const CG_DEFAULT = ["TORTILLAS","PAGOS","OTROS"];
 
 const fmt = (n) => `$${Number(n||0).toLocaleString("en-US",{minimumFractionDigits:2,maximumFractionDigits:2})}`;
